96f41149f6adc3104e0862f78c7a34d443479e4e
fromprophet <1106421540@qq.com>
2023-03-29 14:24:08 +0800

    genie V2.0

--- genie/gamePart.py
+++ genie/gamePart.py
@@ -82,13 +82,18 @@ class Select(object):
         self.font = pygame.font.Font("fonts/国潮招牌字体.ttf", 36)
         self.chessboard = Chessboard("images/chessboard.png").image
         self.mode = "select" # 控制做题阶段。 memory：记忆阶段 select：选择阶段
+        self.mouseindex = None # 控制当前鼠标放在哪个格子上，默认设为None
+        self.temp_property = 0 # 记录单题的正确与否（1对2错0未作答）
+        self.iscomplete = 0 # 记录是否通关（1已通关2未通关）
+        self.clickmode = "none" # 存储点击按钮情况 none无按钮 nextlevel下一题 result进入结算
+        self.playeranswerlist = [] # 记录用户单题作答情况
+        self.wronganswerlist = [] # 如用户点错了把下标放在此列表中
         
         self.__createroles__()
         
     def __createroles__(self):
         self.property_list = ["黄色", "棕色", "蓝色", "红色", "绿色"]
         self.temp_property_list = self.property_list[0:self.level + 1] # 用切片的形式保存当前难度下精灵种类
-        print(self.temp_property_list)
         
         self.descriptionfont = self.font.render("第二阶段：请选择" + str(self.level) + "个  " + self.temp_property_list[self.temp_random_index] + "  精灵      的位置", True, 'black')
         self.descriptionrect = self.descriptionfont.get_rect()
@@ -98,8 +103,54 @@ class Select(object):
         # 创建一个SpriteItem对象，放在题干处
         self.tempsprite = SpriteItem(self.screen, self.temp_random_index)
         
-    def eventhandler(self):
-        pass
+    def eventhandler(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            motionpos = pygame.mouse.get_pos()
+            posx = motionpos[0]
+            posy = motionpos[1]
+            self.mouseindex = self.__getmouseindex__(posx, posy)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            keyarr = pygame.mouse.get_pressed()
+            for index in range(len(keyarr)):
+                if keyarr[index] and index == 0:
+                    keypos = pygame.mouse.get_pos()
+                    keyposx = keypos[0]
+                    keyposy = keypos[1]
+                    if keyposx > 370 and keyposx < 530 and keyposy > 580 and keyposy < 660:
+                        if self.clickmode == "nextlevel":
+                            self.mode = "memory"
+                        elif self.clickmode == "result":
+                            if self.temp_property == 1:
+                                self.iscomplete = 1
+                            elif self.temp_property == 2:
+                                self.iscomplete = 2
+                    else:
+                        keyindex = self.__getmouseindex__(keyposx, keyposy)
+                        if keyindex != None and self.temp_property == 0 and len(self.playeranswerlist) < self.level:
+                                if self.sprite_list[keyindex[0]][keyindex[1]].property == 2:
+                                    self.sprite_list[keyindex[0]][keyindex[1]].property = 1 # 将用户点击的方块翻面
+                                    if self.sprite_list[keyindex[0]][keyindex[1]].index == self.temp_random_index:
+                                        # 如用户点对了(已翻面的不能再次翻面)
+                                        self.playeranswerlist.append([keyindex[0], keyindex[1]])
+                                        if len(self.playeranswerlist) == self.level:
+                                            self.temp_property = 1
+                                    elif self.sprite_list[keyindex[0]][keyindex[1]].index != self.temp_random_index:
+                                        # 如用户点错了
+                                        self.wronganswerlist.append([keyindex[0], keyindex[1]])
+                                        self.temp_property = 2
+                    
+            
+    def __getmouseindex__(self, x, y):
+        # 获取当前鼠标在哪个格子上。
+        res = []
+        i = (y - 150) // 80
+        j = (x - 90) // 80
+        if i >= 0 and j >= 0 and i < self.level and j < 9:
+            res = [i, j]
+        if len(res) != 0:
+            return res
+        else:
+            return None
     
     def update(self):
         # 显示精灵
@@ -116,3 +167,27 @@ class Select(object):
         self.levelrect.top = 0
         self.screen.blit(self.levelfont, self.levelrect)
         self.screen.blit(self.tempsprite.image1, (620, 60))
+        # 显示hover
+        if self.mouseindex != None and self.sprite_list[self.mouseindex[0]][self.mouseindex[1]].property == 2: # 写and后面的条件为了避免翻面后还是显示hover
+            self.screen.blit(pygame.image.load("images/hover.png").convert_alpha(), ((self.mouseindex[1] * 80 + 90), (self.mouseindex[0] * 80 + 150)))
+        # 显示绿框或红框
+        if len(self.playeranswerlist) != 0:
+            for i in range(len(self.playeranswerlist)):
+                pygame.draw.rect(self.screen, "green", ((self.playeranswerlist[i][1] * 80 + 90), (self.playeranswerlist[i][0] * 80 + 150), 80, 80), width = 3)
+        if len(self.wronganswerlist) != 0:
+            for i in range(len(self.wronganswerlist)):
+                pygame.draw.rect(self.screen, "red", ((self.wronganswerlist[i][1] * 80 + 90), (self.wronganswerlist[i][0] * 80 + 150), 80, 80), width = 3)
+        # 显示对勾或叉号
+        if self.temp_property == 1:
+            self.screen.blit(pygame.transform.scale(pygame.image.load("images/right.png").convert_alpha(), (150, 110)), (700, 500))
+            if self.level < 4:
+                self.screen.blit(pygame.transform.scale(pygame.image.load("images/nextlevel.png").convert_alpha(), (160, 80)), (370, 580))
+                self.clickmode = "nextlevel"
+            else:
+                self.screen.blit(pygame.transform.scale(pygame.image.load("images/result.png").convert_alpha(), (160, 80)), (370, 580))
+                self.clickmode = "result"
+        elif self.temp_property == 2:
+            self.screen.blit(pygame.transform.scale(pygame.image.load("images/wrong.png").convert_alpha(), (150, 110)), (700, 500))
+            self.screen.blit(pygame.transform.scale(pygame.image.load("images/result.png").convert_alpha(), (160, 80)), (370, 580))
+            self.clickmode = "result"
+            
--- genie/genie.py
+++ genie/genie.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import random
+import math
+import time
 from gameObject import *
 from gamePart import *
 
@@ -8,10 +10,64 @@ pygame.init()
 pygame.font.init()
 pygame.mixer.init()
 
+class GenieReady(object):
+    # 准备界面
+    def __init__(self):
+        super().__init__()
+        self.screen = pygame.display.set_mode((1200, 700))
+        pygame.display.set_caption("找精灵")
+        self.font = pygame.font.Font("fonts/国潮招牌字体.ttf", 72)
+        self.gametime = 3
+        self.colorlist = [['#0000FF', '#FFFF00'], ['#00FF00', '#0000FF'], ['#FF0000', '#00FF00'], ['#000000', '#808080']]
+        self.second_sound = pygame.mixer.Sound("sounds/second.mp3")
+        self.second_sound.set_volume(0.4)
+        self.starttime = time.time()
+        self.endtime = time.time()
+
+    def startGame(self):
+        self.second_sound.play()
+        while True:
+            self.__enentHandler__()
+            
+            self.__update__()
+            pygame.display.flip()
+
+    def __enentHandler__(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def __timeget__(self):
+        # 通过time.time()读秒
+        self.endtime = time.time()
+        if self.gametime >= 1:
+            if self.endtime - self.starttime >= 1.0:
+                self.gametime -= 1
+                self.starttime = time.time()
+                self.second_sound.play()
+        
+        if self.gametime <= 0:
+            self.second_sound.stop()
+            genie = Genie()
+            genie.startGame()
+
+    def __update__(self):
+        self.screen.fill("#808080")
+        self.screen.blit(pygame.image.load("images/description.png").convert(), (900, 0))
+        text = self.font.render(str(self.gametime), True, (0, 0, 0))
+        textrect = text.get_rect()
+        textrect.center = (400, 350)
+        self.__timeget__()
+        pygame.draw.circle(self.screen, self.colorlist[3 - self.gametime][0], (400, 350), 200, width = 200)
+        pygame.draw.arc(self.screen, self.colorlist[3 - self.gametime][1], (200, 150, 400, 400), math.radians(90), math.radians(1 - (self.endtime - self.starttime) * 360 + 90), width = 400) # rect参数： left, top, width, height
+        self.screen.blit(text, textrect)
+
 class Genie(object):
     def __init__(self):
         super().__init__()
         self.screen = pygame.display.set_mode((1200, 700))
+        pygame.display.set_caption("找精灵")
         self.clock = pygame.time.Clock()
         self.level = 1 # 难度
         self.property_list = ["黄色", "棕色", "蓝色", "红色", "绿色"]
@@ -20,8 +76,6 @@ class Genie(object):
         self.__createroles__()
         
     def __createroles__(self):
-        # spriteNum = self.level + 1 # 每个难度下显示精灵的种数
-        # self.temp_index_list = self.property_list[0:self.level + 1] # 用切片的形式保存当前难度下精灵种类
         self.temp_random_index = random.randint(0, self.level) # 随机该题答案
         self.index_list = [] # 保存所有精灵的index
         self.sprite_list = [] # 保存所有SpriteItem类
@@ -36,7 +90,6 @@ class Genie(object):
         print(self.temp_random_index, self.index_list, flag)
         # 判断出题合理后，给所有精灵赋值
         self.sprite_list = self.__set_sprite_list__()
-        
         # 题出完了给两个gamePart的类赋值
         self.memory = Memory(self.screen, self.sprite_list, self.level)
         self.select = Select(self.screen, self.sprite_list, self.level, self.temp_random_index)
@@ -90,12 +143,30 @@ class Genie(object):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if self.mode == "memory":
-                self.memory.eventhandler(event)
-                
             if self.memory.mode == "select":
                 self.mode = "select"
                 self.__turnover__()
+                self.memory.mode = "memory" # 这里把Memory类的mode属性改回去，以避免重复判断
+            if self.select.mode == "memory":
+                self.mode = "memory"
+                self.select.mode = "select"
+                self.level += 1
+                self.__createroles__() # 出新题    
+                
+            if self.select.iscomplete != 0:
+                if self.select.iscomplete == 1:
+                    result = Result(self.level, True) # 已通关
+                elif self.select.iscomplete == 2:
+                    result = Result(self.level, False) # 未通关
+                self.select.iscomplete = 0
+                result.startGame() # 对于是否进入result界面的判断需要放在game类不能放在别的文件，否则将引起两个文件互相导引发死锁，程序闪退
+                
+            if self.mode == "memory":
+                self.memory.eventhandler(event)
+            if self.mode == "select":
+                self.select.eventhandler(event)
+                
+            
                 
     def __update__(self):
         self.screen.fill("#808080")
@@ -103,6 +174,7 @@ class Genie(object):
         if self.memory.mode == "select":
             self.mode = "select" # 因为切到select界面有鼠标操作和倒计时两种方法，所以需要写两次。这里不写切界面会有延迟
             self.__turnover__()
+            self.memory.mode = "memory" # 这里把Memory类的mode属性改回去，以避免重复判断
             
         if self.mode == "memory":
             self.memory.update()
@@ -115,10 +187,62 @@ class Genie(object):
         for i in range(len(self.sprite_list)):
             for j in range(len(self.sprite_list[i])):
                 self.sprite_list[i][j].property = 2
+                
+class Result(object):
+    # 显示结果
+    def __init__(self, level, iscomplete):
+        super().__init__()
+        self.screen = pygame.display.set_mode((1200, 700))
+        pygame.display.set_caption("找精灵")
+        self.font = pygame.font.Font("fonts/国潮招牌字体.ttf", 48)
+        self.sum = sum
+        self.level = level
+        self.iscomplete = iscomplete # 是否通关
+        self.__setfonts__()
+        
+    def __setfonts__(self):
+        if self.iscomplete == True:
+            self.text1 = self.font.render("恭喜通关！", True, (0, 0, 0))
+            self.text2 = self.font.render("已完成所有难度", True, (0, 0, 0))
+        elif self.iscomplete == False:
+            self.text1 = self.font.render("任务结束", True, (0, 0, 0))
+            self.text2 = self.font.render("已达到难度" + str(self.level), True, (0, 0, 0))
+        
+    def startGame(self):
+        while True:
+            self.__eventhandler__()
+            self.__update__()
+            pygame.display.flip()
+
+    def __eventhandler__(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pressedArr = pygame.mouse.get_pressed()
+                for index in range(len(pressedArr)):
+                    if index == 0 and pressedArr[index]:
+                        pos = pygame.mouse.get_pos()
+                        posX = pos[0]
+                        posY = pos[1]
+                        if posX > 500 and posX < 700 and posY > 450 and posY < 550:
+                            ready = GenieReady()
+                            ready.startGame()
+
+    def __update__(self):
+        self.screen.fill("#808080")
+        self.screen.blit(self.text1, (150, 150))
+        self.screen.blit(self.text2, (150, 250))
+        self.screen.blit(pygame.image.load("images/restart.png").convert_alpha(), (500, 450))
+
         
 def genieGame():
-    genie = Genie()
-    genie.startGame()
+    # genie = Genie()
+    # genie.startGame()
+    ready = GenieReady()
+    ready.startGame()
     
 if __name__ == "__main__":
     genieGame()
